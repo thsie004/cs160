18c85e0e96636647c11969b6dbd54fca51891068
Tung Hsieh <thsie004@foxtrot-14.cs.ucr.edu>
2017-03-13 18:51:41 -0700

    finished problem 1

--- p1.c
+++ p1.c
@@ -2,20 +2,17 @@
 #include "stdio.h"
 #include "omp.h"
 
-int main(){
+int main(int argc, char* argv[]){
+    int i, n;
 
-/**
-    omp_set_num_threads(4);
-#pragma omp parallel
-{
-    int ID = omp_get_thread_num();
-    printf( "hello(%d)", ID);
-    printf( "world(%d)\n", ID);
-}
-*/
+    //Checks for argument
+    if(argc != 2){
+        printf("Missing the number of threads as argument\n");
+    }else if( !( n = atoi(argv[1]) ) ){
+	printf("Invalid argument, please enter a valid number\n");
+    }
 
-    int i;
-    double x, sPi, ompPi, sum = 0.0, step;
+    double x, sPi = 0, ompPi = 0, sum = 0.0, step;
     const static long num_steps = 100000;
 
     //Sequential vertion of pi
@@ -29,23 +26,30 @@ int main(){
 
     sPi = step * sum;
 
-    printf("Sequential pi: %0.10f in %f time\n", sPi, omp_get_wtime()-start);
+    printf("Sequential pi is %0.10f calculated in %f seconds\n", sPi, omp_get_wtime()-start);
     //end of sequential pi
 
-
-    sum = 0.0;
-    
     //OpenMP version of pi
     start = omp_get_wtime();
-#pragma omp parallel num_threads(20)
-    for(i = 0; i < num_steps; i++){
-        x = (i + 0.5) * step;
-        sum += 4.0 / (1.0 + x * x);
+    omp_set_num_threads(n);     
+#pragma omp parallel
+{
+    int ID = omp_get_thread_num(), I;
+    int start = (int) ((double) ID / (double) n * num_steps); 
+    int end = (int) ((double) (ID+1) / (double) n * num_steps);
+    double storage = 0.0, X;
+
+    for(I = start; I < end; I++){
+        X = (I + 0.5) * step;
+        storage += 4.0 / (1.0 + X * X);
     }
+#pragma omp atomic
+    ompPi += storage;
+}
+    
+    ompPi *= step;
 
-    ompPi = step * sum;
-
-    printf("OpenMP pi: %0.10f in %f time\n", ompPi, omp_get_wtime()-start);
+    printf("OpenMP pi is %0.10f calculated in %f seconds.\n", ompPi, omp_get_wtime()-start);
     //end of OpenMP pi
     return 0;
 }
